af1413ddfdd4f7f06d6c344691ca4aeb4e66471a
Chris Gonzales <cm.gonzales@yahoo.com>
2018-09-13 20:54:58 -0700

    added bootrap and more descriptions

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 var app = angular.module('weather', []);
 app.controller('myCtrl', function($scope, $http) {
     $scope.zip = "";
@@ -14,11 +12,17 @@ app.controller('myCtrl', function($scope, $http) {
     .then(function successCallback (response) {
         $scope.temp = Math.floor(response.data.main.temp * 9/5 - 459.67) + "º F";
         let e = response.data.weather;
-        for(var i = 0; i < response.data.weather.length; i++){
-            if(e[i].description == "overcast clouds"){
-                $scope.img = "images2.jpg"
-            }
+        for(var i = 0; i < e.length; i++){
+        
+        var forecast = e[i].description.split(" ");    
+        for(var q = 0; q < forecast.length; q++){
+             forecast[q] = forecast[q].substring(0,1).toUpperCase() + forecast[q].substring(1);   
         }
+        forecast = forecast.join(" ")
+        $scope.description =  "Forecast: " + forecast;
+        }
+        $scope.mintemp = "Minimum Temp: " + Math.floor(response.data.main.temp_min * 9/5 - 459.67) + "º F";
+        $scope.maxtemp = "Maximum Temp: " + Math.floor(response.data.main.temp_max * 9/5 - 459.67) + "º F";
         return response.data;
     }, function errorCallback (response) {
         return response;
