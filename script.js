612530988758983bed4ab15718f7aa2241a8343a
Chris Gonzales <cm.gonzales@yahoo.com>
2018-09-17 19:16:16 -0700

    changed font family

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ app.controller('myCtrl', function($scope, $http) {
     $scope.datas = function(){
         
      var url = "http://api.openweathermap.org/data/2.5/weather?zip=" + $scope.zip + ",us&APPID=764126d68f249066554911a9a05af466"
-    
+
      $http({
         method : "GET",
         url : url
